7298efaeebad31df5397c7cca62ef292889271a1
mohamedellithyyyy <ellithy201102@gmail.com>
2025-09-04 11:38:05 +0300

    D_Difference.cpp solution

--- Problem-Solving/Assiut-University-Training-Newcomers/Sheet_1_Data-type-Conditions/D_Difference.cpp
+++ Problem-Solving/Assiut-University-Training-Newcomers/Sheet_1_Data-type-Conditions/D_Difference.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    // Problem: Difference
-    
+ 
+int main() 
+{
+    long long A,B,Z,D;
+    cin>>A>>B>>Z>>D;
+    long long Difference = A*B - Z*D;
+    cout<< "Difference = "<<Difference;
     return 0;
 }
