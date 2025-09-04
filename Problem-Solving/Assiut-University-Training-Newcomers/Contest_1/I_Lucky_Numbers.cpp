e37cbd9bc7314176c4dab00fa0669a5a18107cb3
mohamedellithyyyy <ellithy201102@gmail.com>
2025-09-04 12:11:25 +0300

    solved I_Lucky_Numbers.cpp

--- Problem-Solving/Assiut-University-Training-Newcomers/Contest_1/I_Lucky_Numbers.cpp
+++ Problem-Solving/Assiut-University-Training-Newcomers/Contest_1/I_Lucky_Numbers.cpp
@@ -2,7 +2,20 @@
 using namespace std;
 
 int main() {
-    // Problem: Lucky Numbers
-    
+    int N;
+    cin >> N;
+
+    int last_digit = N % 10;          // last digit
+    int second_last_digit = (N / 10) % 10;  // second from right
+    // Check divisibility safely
+    bool check1 = (second_last_digit != 0 && last_digit % second_last_digit == 0);
+    bool check2 = (last_digit != 0 && second_last_digit % last_digit == 0);
+
+    if(check1 || check2){
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+
     return 0;
 }
